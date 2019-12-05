9c2f60c99958b828c03860dd65a4fe59e6c87561
vvxforce <vvxforce@gmail.com>
2019-12-05 09:38:22 +0200

    Updates

---
+++ precache-manifest.992439a624b4d298194c1c6488129aa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1ae6832e4dfdb7d5e9f238a66e598f",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "75d51c5f2d9074b4a3ae",
+    "url": "/react_static-list-of-todos/static/css/main.2997b155.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "75d51c5f2d9074b4a3ae",
+    "url": "/react_static-list-of-todos/static/js/main.b54bb0fe.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.992439a624b4d298194c1c6488129aa0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_static-list-of-todos/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
